b9064fb653bdc65272b38233e4a28aa2aa92fc6a
suwendri.wijaya <suwendri.wijaya@esb.co.id>
2025-08-02 09:06:05 +0700

    Refactor genai function to improve parameter handling and model selection logic

--- genai.js
+++ genai.js
@@ -7,27 +7,25 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-export default async function main(prompt = '', imgBase64 = '', mimeType = '') {
+export default async function genai(prompt, attachmentBase64, mimeType) {
+  let modelName;
   let contents = [];
-  if (!prompt) {
-    return "No prompt provided.";
-  } else {
-    contents.push({
-      text: prompt,
-    });
-  }
+  contents.push({ text: prompt });
 
-  if (imgBase64 && mimeType) {
+  if (attachmentBase64 && mimeType) {
+    modelName = process.env.GEMINI_PRO_MODEL;
     contents.push({
       inlineData: {
         mimeType: mimeType,
-        data: imgBase64,
+        data: attachmentBase64,
       },
     });
+  } else {
+    modelName = process.env.GEMINI_FLASH_MODEL;
   }
 
   const response = await ai.models.generateContent({
-    model: process.env.GEMINI_MODEL,
+    model: modelName,
     contents: contents,
   });
 
